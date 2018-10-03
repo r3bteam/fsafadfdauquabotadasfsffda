3616844164f744fb8b2dbb7740c722e35fdca989
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-10-02 22:26:53 -0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager(`./sysop.js`, { totalShards: 2});
+const manager = new ShardingManager(`sysop.js`, { totalShards: 2});
 
 manager.spawn();
 
