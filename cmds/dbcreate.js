18a5e4f852cd4f6303698a6856a20c152e3d5c9c
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-10-14 00:25:05 -0300

    Update dbcreate.js

--- cmds/dbcreate.js
+++ cmds/dbcreate.js
@@ -5,8 +5,8 @@ if(!['244489368717230090'].includes(message.author.id) return;
 if(!args) return message.channel.send(`${message.author} Defina ID da guilda que criarei um documento.`)
 
 let createAccount = args => {
-                let server = new db.Guilds({
-                    _id: id,
+                let server = new args db.Guilds({
+                    _id: args,
                     "setprefix": "sy!"
                 });
 
