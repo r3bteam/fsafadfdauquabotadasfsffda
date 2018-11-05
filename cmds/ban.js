06f14197c20bd442a76e17ee7ee4885b03e7df19
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-11-05 02:13:37 -0200

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -4,14 +4,37 @@ var dayCol = new Set()
 
 exports.run = (Sysop, message, suffix) => {
   
+  
+  db.Guilds.findOne({
+    "_id": message.guild.id}, 
+    function(erra, servidor) {
+        if (servidor) {
+
+  
             let args = suffix 
             let sysop =  args.slice(1).join(' ') 
             ? args.slice(1).join(' ')
-            :  `Foi banido por ${message.author.username}#${message.author.discriminator}. Motivo não especificado.`;
+            :  `Foi banido por ${message.author.username}#${message.author.discriminator}. Sem motivo.`;
 
 
 
-if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('<:xguardian:476061993368027148> Desculpe, você não tem permissão de banir usuário neste servidor!');
+
+if (!message.member.hasPermission('BAN_MEMBERS')) 
+return message.channel.send(`<:alert:506568192521535498> **|** ${message.author}, você não tem permissão para banir, bobinho(a)!`);
+
+const filtro = ['discord.li','discord.io','discord.gg','https://','filho da puta','corno','corna','gay','viadinho', 'fdp', 'cú', 'filho da mãe', 'baka', 'idiota', 'tnc', 'puta', 'puto', 'piranha', 'anta', 'bosta', 'merda', 'penis','buceta','ppk','pênis','anûs','anus','vaca','puta de luxo','k7']
+
+if(filtro.some(p => message.content.includes(p))){
+  message.channel.send(`<:alert:506568192521535498> ${message.author} você não pode banir este usuário pois está usando linguagens impróprias como motivo.`);
+  return;
+}  
+
+let searchChannel = message.guild.channels.find(search => search.id === servidor.BAN);
+
+if (!searchChannel)
+return message.channel.send(`<:alert:506568192521535498> **|** Opa ${message.author}, nenhum canal para o logs dos banimentos foi definido. Use: ${servidor.setprefix}banlog para ver mais informações.`);
+        
+
 let id = message.mentions.users.first()
 
     ? message.mentions.users.first().id 
@@ -19,37 +42,37 @@ let id = message.mentions.users.first()
     : args
     ? args[0]
     : null;
-if (!id) return message.reply('<:sysalerta:469789950938841088> Mencione alguém ou especifique um ID.');
+if (!id) return message.reply('<:Alerta:507449197801504778> **|** Mencione um usuário ou especifique um ID.');
 
 let banPerms = message.guild.member(Sysop.user).hasPermission('BAN_MEMBERS')
-if (!banPerms)  return message.reply("Eu não tenho permissão para banir usuários nesse servidor.");
+if (!banPerms)  return message.reply("<:Nao:507411636664139786> **|** Eu não tenho permissão para banir usuários nesse servidor.");
 
 let user = Sysop.users.has(id) ? Sysop.users.get(id) : null;
 
-if (!user) return message.reply("<:xguardian:476061993368027148> Não encontrei nenhum usuário.")
+if (!user) return message.reply("<:Alerta:507449197801504778> **|** Não encontrei nenhum usuário.");
 
 
 
-message.channel.send(`<:sysalerta:469789950938841088> **|** Opa ${message.author} Essa função requer confirmação. Responda essa mensagem com: **"banir"** para banir o usuário e **"cancelar"** para cancelar o banimento.`).then(() => {
+message.channel.send(`<:Alvo:507455947388747778> **|** Opa ${message.author}, essa função requer confirmação. Responda essa mensagem com **"ban"** para banir o usuário ou com **"cancelar"** para cancelar o banimento.`).then(() => {
 message.channel.awaitMessages(res => 
-(res.content == "banir" && res.author.id == message.author.id) || (res.content == 'cancelar' && res.author.id == message.author.id), { 
+(res.content == "ban" && res.author.id == message.author.id) || (res.content == 'cancelar' && res.author.id == message.author.id), { 
 
 max: 1, time: 60000, errors: ['time'] }).then(col => {
     
-if (col.first().content == 'banir') {
+if (col.first().content == 'ban') {
 
 message.guild.fetchBans().then(bans => {
 let users = bans.filter(r => r === user);
 if (users.first()) 
-return message.channel.send(`<:xguardian:476061993368027148> ${message.author} este usuário já está banido aqui.`);
+return message.channel.send(`<:Cartas:507449197755236363> **|** ${message.author} este usuário já se encontra banido.`);
 
     
 let bannable = message.guild.member(id)
 if (bannable) {  
-if (bannable.highestRole.position >= message.member.highestRole.position) return message.reply("<:xguardian:476061993368027148> Você não pode banir este usuário pois seu cargo é menor ou igual a o do usuário a ser banido!")
+if (bannable.highestRole.position >= message.member.highestRole.position) return message.reply("<:Nao:507411636664139786> **|** Você não tem permissão suficiente para banir esse usuário!")
     
 if (!message.guild.member(user).bannable) 
-return message.reply('<:xguardian:476061993368027148> Eu não posso banir esse usuário.');
+return message.channel.send(`<:Nao:507411636664139786> **|** ${message.author}, eu não posso banir esse usuário.`);
     
 } else {
 
@@ -57,13 +80,14 @@ return message.reply('<:xguardian:476061993368027148> Eu não posso banir esse u
         let server = message.guild
     const embed1 = new Discord.RichEmbed()
     .setTitle(`Ban Ação`)
-    .addField(`<:banSysop:476264254606016515> Usuário banido`, user)
-    //.addField(`**Motivo:**`, sysop)
+    .addField(`Usuário banido`, user)
+    .addField(`**Motivo:**`, sysop)
     .addField(`Banido do servidor:`, server.name)
     .addField(`**Executor**`, message.author)
     .setThumbnail(user.avatarURL)
     .setColor('#00fffa')
-    message.channel.send({embed1});
+    Sysop.guilds.get(message.guild.id).channels.get(servidor.logg_banAction).send({embed1})
+    message.channel.send(`<:Adaga:507454316479774730> **|** ${message.author} usuário banido com sucesso!`)
     user.send({embed1})
 }
 if (user) {
@@ -73,17 +97,17 @@ if (user) {
         let server = message.guild
     const embed = new Discord.RichEmbed()
     .setTitle(`Ban Ação`)
-    .addField(`<:banSysop:476264254606016515>  Usuário banido`, user)
-    //.addField(`**Motivo:**`, sysop)
+    .addField(`Usuário banido`, user)
+     .addField(`**Motivo:**`, sysop)
     .addField(`Banido do servidor:`, server.name)
     .addField(`**Executor**`, message.author)
     .setThumbnail(user.avatarURL)
     .setColor('#00fffa')
-    message.channel.send({embed});
+    Sysop.guilds.get(message.guild.id).channels.get(servidor.BAN).send({embed})
     user.send({embed})
 
 } else {
-    message.reply('<:sysalerta:469789950938841088> Não encontrei nenhum usuário');
+    message.reply('<:Alerta:507449197801504778> **|** Não consegui encontrar nenhum usuário.');
 
 }
 })
@@ -91,9 +115,10 @@ if (user) {
     }
     if (col.first().content == 'cancelar') {
  
-    message.channel.send(`OK ${message.author}! Você cancelou o banimento de: ${user}`);
+    message.channel.send(`<:Sim:507411636215087115> **|** ${message.author} sua solicitação de banimento foi cancelada!`);
     }
-}).catch(() => message.channel.send(`:shrug::skin-tone-2: **|** Tempo de **1** minuto passado. Solicitação de banimento expirada.`));
+}).catch(() => message.channel.send(`<:Tempo:507450275594764298> **|** ${message.author}, o seu tempo de **1 minuto** terminou. Solicitação de banimento expirada.`));
       
 });
+}});
 };
